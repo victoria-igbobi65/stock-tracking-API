b3f44bee7bf4509fcf1af58e3c604ef67556c015
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-01 20:00:51 +0100

    refactor: added Port from env

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -21,6 +22,6 @@ async function bootstrap() {
             },
         }),
     );
-    await app.listen(3000);
+    await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 5000);
 }
 bootstrap();
