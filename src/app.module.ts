e601d533c8013f18c562db77c4693b58e05c4ea8
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-01 06:36:40 +0100

    new: first commit

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import * as Joi from 'joi';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '.env',
+            validationSchema: Joi.object({
+                PORT: Joi.number().default(4000),
+                NODE_ENV: Joi.string().default('development'),
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.number().required(),
+                DB_USERNAME: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+                DB_DATABASE: Joi.string().required(),
+                DB_SSL: Joi.boolean().required(),
+            }),
+        }),
+        AuthModule,
+        DatabaseModule,
+        UsersModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    app.use(helmet());
+    app.enableCors({
+        origin: '*',
+    });
+    app.setGlobalPrefix('api/v1');
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            transformOptions: {
+                enableImplicitConversion: true,
+            },
+        }),
+    );
+    await app.listen(3000);
+}
+bootstrap();
