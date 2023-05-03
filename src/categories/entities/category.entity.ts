dfacc65e50f59ee92854c07fa71fb6a5cd4fd9ac
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-03 13:08:45 +0100

    new: added relationship between category and inventory

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,9 +1,13 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 
 import { BaseEntity } from 'src/common/entities/base.entity';
+import { Inventory } from 'src/inventory/entities/inventory.entity';
 
 @Entity('categories')
 export class Category extends BaseEntity {
     @Column({ type: 'varchar', unique: true })
     name: string;
+
+    @OneToMany(() => Inventory, (inventory) => inventory.category)
+    inventories: Inventory[];
 }
