dfd8ae12c43bbd1d6d7f3d422f02e427ad255035
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-05 22:55:41 +0100

    new: updated updateInventory inventory dto

--- src/inventory/dto/update-inventory.dto.ts
+++ src/inventory/dto/update-inventory.dto.ts
@@ -1,4 +1,39 @@
+import {
+    IsDefined,
+    IsNotEmpty,
+    IsString,
+    IsPositive,
+    IsNumber,
+} from 'class-validator';
 import { PartialType } from '@nestjs/swagger';
 import { CreateInventoryDto } from './create-inventory.dto';
 
-export class UpdateInventoryDto extends PartialType(CreateInventoryDto) {}
+export class UpdateInventoryDto extends PartialType(CreateInventoryDto) {
+    @IsString()
+    @IsDefined()
+    @IsNotEmpty()
+    readonly category?: string;
+
+    @IsString()
+    @IsDefined()
+    @IsNotEmpty()
+    readonly description?: string;
+
+    @IsDefined()
+    @IsNotEmpty()
+    @IsPositive()
+    @IsNumber()
+    readonly costPrice?: number;
+
+    @IsDefined()
+    @IsNotEmpty()
+    @IsPositive()
+    @IsNumber()
+    readonly quantityInStock?: number;
+
+    @IsDefined()
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
+    readonly sellingPrice?: number;
+}
