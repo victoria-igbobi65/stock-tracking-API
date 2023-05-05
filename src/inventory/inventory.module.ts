48259d3a579d0f171ce0ac58fd97102f50f7e78e
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-05 08:50:17 +0100

    new: added categories module

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -3,9 +3,14 @@ import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from 'src/categories/entities/category.entity';
+import { Inventory } from './entities/inventory.entity';
+import { CategoriesModule } from 'src/categories/categories.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Category])],
+    imports: [
+        TypeOrmModule.forFeature([Category, Inventory]),
+        CategoriesModule,
+    ],
     controllers: [InventoryController],
     providers: [InventoryService],
 })
